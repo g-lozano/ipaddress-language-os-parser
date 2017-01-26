a8982ba55bcc71e6d9c1343a9c6c915249b64700
G Lozano <gfourth1005@gmail.com>
2017-01-26 22:14:41 +0000

    commmit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express()
+var whoami = require('whoami')
+var port = 8080
+
+app.get('/', function (req, res) {
+    var language = req.headers["accept-language"].split(',')[0]
+    var ipaddress = req.headers["x-forwarded-for"]
+    var os = req.headers["user-agent"].split('(')
+    os.shift()
+    os = os.join('').split(')').shift()
+    
+    var json = {
+        ipaddress: ipaddress,
+        language: language,
+        software: os
+    }
+    
+    res.send(json)
+})
+
+app.listen(process.env.PORT || port)
